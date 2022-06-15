f1b44ae2c841a98552189ecf40b6f3b471b267af
K3rnelP4n1k <Phreno@users.noreply.github.com>
2022-06-15 12:50:04 +0000

    ajoute la possibilite de tourner à gauche

--- langton/spec/Langton.js
+++ langton/spec/Langton.js
@@ -10,7 +10,7 @@ module.exports = function Langton() {
 
     self.grille = [];
     self.historique = [
-        { x: 0, y: 0, direction: self.HAUT }
+        { x: -1, y: 0 }
     ];
     self.x = 0;
     self.y = 0;
@@ -20,12 +20,28 @@ module.exports = function Langton() {
         return self.grille.find(cell => cell.x === self.x && cell.y === self.y)
     }
 
-    self.voisinage = () => [
-        { x: self.x - 1, y: self.y }, // gauche
-        { x: self.x, y: self.y - 1 }, // haut
-        { x: self.x + 1, y: self.y }, // droite
-        { x: self.x, y: self.y + 1 } // bas
+    self.voisinage = [
+        { x: - 1, y: 0 }, // gauche
+        { x: 0, y: - 1 }, // haut
+        { x: 1, y: 0 }, // droite
+        { x: 0, y: 1 } // bas
     ]
 
+    self.offset = () => self.historique.slice(-1)[0]
+
+    self.provenance = () =>
+        self.voisinage
+            .findIndex(voisin => voisin.x === self.x + self.offset().x && voisin.y === self.y + self.offset().y)
+
+    self.avancer = () => {
+        switch(self.provenance()){
+            case self.GAUCHE:
+                self.y += self.estSurUneCaseNoire() ? 1 : -1
+                break;
+        }
+    }
+
     return self;
+
+
 }
--- langton/spec/LangtonSpec.js
+++ langton/spec/LangtonSpec.js
@@ -19,30 +19,24 @@ describe("Langton", function () {
     })
 
     it("doit connaitre la position et la direction de la fourmi", () => {
-        expect(langton.x).toBeDefined();
-        expect(langton.y).toBeDefined();
+        expect(langton.x).toBe(0);
+        expect(langton.y).toBe(0);
         expect(langton.direction).toBeDefined();
     })
 
     it("doit pouvoir dire si la fourmi est sur une case noire", () => {
         langton.grille = [{ x: 0, y: 0 }];
-        langton.x = 0;
-        langton.y = 0;
         expect(langton.estSurUneCaseNoire()).toBeTruthy();
     })
 
     it("doit tourner à gauche si la case est blanche", () => {
         langton.grille = [];
-        langton.x = 0;
-        langton.y = 0;
         expect(langton.estSurUneCaseNoire()).toBeFalsy();
         expect(langton.direction()).toBe('gauche');
     })
 
     it("doit tourner à droite si la case est noire", () => {
         langton.grille = [{ x: 0, y: 0 }];
-        langton.x = 0;
-        langton.y = 0;
         expect(langton.estSurUneCaseNoire()).toBeTruthy();
         expect(langton.direction()).toBe('droite');
     })
@@ -50,10 +44,6 @@ describe("Langton", function () {
 
 
     describe("pour tourner", () => {
-        beforeEach(() => {
-            langton.x = 0;
-            langton.y = 0;
-        })
         // Exemple avec les coordonnées (x, y).
         // Fourmi sur la case (0, 0)
         // ------------------------
@@ -62,7 +52,7 @@ describe("Langton", function () {
         // -1, 1    | 0, 1  | 1, 1
         // ========================
         it("doit pouvoir localiser les cases voisines", () => {
-            expect(langton.voisinage()).toEqual([
+            expect(langton.voisinage).toEqual([
                 { x: -1, y: 0 },
                 { x: 0, y: -1 },
                 { x: 1, y: 0 },
@@ -71,14 +61,47 @@ describe("Langton", function () {
 
         })
 
-        it("doit historiser les positions de la fourmi", () => {
-            expect(langton.historique).toEqual([{x: 0, y: 0, direction: langton.HAUT}]);
+        it("doit historiser les positions de la fourmi", () =>
+            expect(langton.historique)
+                .toEqual([{ x:-1, y: 0 }])
+        )
+
+        describe("en venant de la gauche (-1, 0)", () => {
+            // ------------------------
+            // -1, -1   | 0, -1 | 1, -1
+            // -1, 0    |[0, 0] | 1, 0
+            // -1, 1    | 0, 1  | 1, 1
+            // ========================
+
+            it("doit retourner l'offset de la case précédente", () => {
+                expect(langton.offset()).toEqual({ x: -1, y: 0 });
+            })
+
+            it("doit déterminer la provenance", ()=>{
+                expect(langton.provenance()).toEqual(langton.GAUCHE);
+            })
+
+            it("doit tourner à droite si la case est noire (0, 1)", ()=>{
+                langton.grille = [{ x: 0, y: 0 }];
+                langton.avancer();
+                expect(langton.x).toBe(0);
+                expect(langton.y).toBe(1);
+            })
+
+            it("doit tourner à gauche si la case est blanche (0, -1)", ()=>{
+                langton.grille = [];
+                langton.avancer();
+                expect(langton.x).toBe(0);
+                expect(langton.y).toBe(-1);
+            })
         })
 
 
-        // venant de la gauche (-1, 0)
+
         // - si elle tourne à gauche, elle doit se trouver sur la case (-1, 0)
         // - si elle tourne à droite, elle doit se trouver sur la case (1, 0)
+
+
         // venant de la droite (1, 0)
         // - si elle tourne à gauche, elle doit se trouver sur la case (1, 0)
         // - si elle tourne à droite, elle doit se trouver sur la case (-1, 0)
